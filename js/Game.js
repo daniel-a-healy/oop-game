92845ab74ae2c46c373e64ac32283772133f1ad0
Daniel Healy <daniel.addison.healy@gmail.com>
2020-10-24 08:55:05 -0400

    Implemented Game constructor, start and randphrase

--- js/Game.js
+++ js/Game.js
@@ -4,12 +4,25 @@
 
 class Game {
 
-    startGame() {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [new Phrase("How are you"), 
+                        new Phrase("Thank you very much"),
+                        new Phrase("Cowabunga dude"),
+                        new Phrase("Pineapple pizza"),
+                        new Phrase("A bird in hand is worth two in the bush")];
+        this.activePhrase = null;
+    }
 
+    startGame() {
+        document.getElementById("overlay").style.display = "none";
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
     }
 
     getRandomPhrase() {
-
+        const randomIndex = Math.floor(Math.random() * Math.floor(this.phrases.length));
+        return this.phrases[randomIndex];
     }
 
     handleInteraction() {
@@ -25,6 +38,6 @@ class Game {
     }
 
     gameOver() {
-        
+
     }
 }
