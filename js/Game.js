a3b2712293d8dc51f1e5feaeb87fa50981ba7625
Daniel Healy <daniel.addison.healy@gmail.com>
2020-10-24 08:32:22 -0400

    Added skeleton for Game and Phrase class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+
+    startGame() {
+
+    }
+
+    getRandomPhrase() {
+
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+        
+    }
+}
